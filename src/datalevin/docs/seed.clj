d025e07637bab17609152cba27d73fa47c0a591c
Huahai Yang <huahai.yang@gmail.com>
2026-03-15 14:45:21 -0700

    fix seed

--- src/datalevin/docs/seed.clj
+++ src/datalevin/docs/seed.clj
@@ -8,6 +8,15 @@
 
 (def db-path (:db-path (config/resolve-env {})))
 
+(defn- example-tx
+  [author-id doc-section code]
+  {:example/id (UUID/randomUUID)
+   :example/code code
+   :example/author [:user/id author-id]
+   :example/doc-section doc-section
+   :example/removed? false
+   :example/created-at (Date.)})
+
 (defn seed []
   (println "Seeding database...")
   
@@ -47,55 +56,25 @@
     (println "  - demo@example.com / password123 (role: user)")
     (println "  - admin@example.com / admin123 (role: admin)")
     
-    (let [examples [{:example/id (UUID/randomUUID)
-                     :example/title "Basic Datalog Query"
-                     :example/description "Simple query to find all users"
-                     :example/code "(d/q '[:find ?e\n  :where [?e :user/name]]\n  conn)"
-                     :example/output "#{[:user/name \"Alice\"] [:user/name \"Bob\"]}"
-                     :example/author [:user/id test-user-id]
-                     :example/category :example/datalog
-                     :example/doc-section "09-datalog-fundamentals"
-                     :example/created-at (Date.)}
-                    
-                    {:example/id (UUID/randomUUID)
-                     :example/title "Transaction with Schema"
-                     :example/description "Transacting data with schema types"
-                     :example/code "(d/transact! conn [{:user/email \"alice@example.com\"\n                         :user/name \"Alice\"\n                         :user/age 30}])"
-                     :example/output "{:db-before ..., :db-after ..., :tx-data [...]}"
-                     :example/author [:user/id demo-user-id]
-                     :example/category :example/datalog
-                     :example/doc-section "07-transactions"
-                     :example/created-at (Date.)}
-                    
-                    {:example/id (UUID/randomUUID)
-                     :example/title "Key-Value Put/Get"
-                     :example/description "Basic key-value operations"
-                     :example/code "(d/transact! conn [[:put {:db/ident :kv\n                    :key \"mykey\"\n                    :value \"myvalue\"}]])\n\n(d/get-value conn :kv \"mykey\")"
-                     :example/output "\"myvalue\""
-                     :example/author [:user/id admin-user-id]
-                     :example/category :example/kv
-                     :example/doc-section "06-key-value-store"
-                     :example/created-at (Date.)}
-                    
-                    {:example/id (UUID/randomUUID)
-                     :example/title "Full-Text Search"
-                     :example/description "Searching with tokenized strings"
-                     :example/code "(d/q '[:find ?e\n  :in $ ?search\n  :where [?e :doc/content ?content]\n  [(fulltext $ :doc/content ?search) [[?e ?match]]]]\n  conn \"database\")"
-                     :example/output "#{[... :doc/content \"...database...\"]}"
-                     :example/author [:user/id test-user-id]
-                     :example/category :example/search
-                     :example/doc-section "17-full-text-search"
-                     :example/created-at (Date.)}
-                    
-                    {:example/id (UUID/randomUUID)
-                     :example/title "Vector Similarity Search"
-                     :example/description "Find similar embeddings"
-                     :example/code "(d/q '[:find ?e ?score\n  :in $ ?query-vector [?limit]\n  :where [?e :embedding/vector ?vec]\n  [(.score (datalevin.search/similarity-search ?vec ?query-vector)) ?score]]\n  db query-vec 10)"
-                     :example/output "#{[entity-id 0.95] [entity-id 0.87]}"
-                     :example/author [:user/id demo-user-id]
-                     :example/category :example/vector
-                     :example/doc-section "18-vector-search"
-                     :example/created-at (Date.)}]]
+    (let [examples [(example-tx test-user-id
+                                "09-datalog-fundamentals"
+                                "(d/q '[:find ?e\n  :where [?e :user/name]]\n  conn)")
+
+                    (example-tx demo-user-id
+                                "07-transactions"
+                                "(d/transact! conn [{:user/email \"alice@example.com\"\n                         :user/name \"Alice\"\n                         :user/age 30}])")
+
+                    (example-tx admin-user-id
+                                "06-key-value-store"
+                                "(d/transact! conn [[:put {:db/ident :kv\n                    :key \"mykey\"\n                    :value \"myvalue\"}]])\n\n(d/get-value conn :kv \"mykey\")")
+
+                    (example-tx test-user-id
+                                "17-full-text-search"
+                                "(d/q '[:find ?e\n  :in $ ?search\n  :where [?e :doc/content ?content]\n  [(fulltext $ :doc/content ?search) [[?e ?match]]]]\n  conn \"database\")")
+
+                    (example-tx demo-user-id
+                                "18-vector-search"
+                                "(d/q '[:find ?e ?score\n  :in $ ?query-vector [?limit]\n  :where [?e :embedding/vector ?vec]\n  [(.score (datalevin.search/similarity-search ?vec ?query-vector)) ?score]]\n  db query-vec 10)")]]
       
       (d/transact! conn examples)
       (println "Created" (count examples) "example submissions"))
